236a747d91aa7684e724c9900c46de415c522bd8
HipsterBrown <headhipster@hipsterbrown.com>
2019-07-06 12:23:27 -0400

    refactor: use debug in place of console.log

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const ambient = require('ambient-attx4');
 
 const MulticastEmitter = require('multicast-eventer');
 
+const debug = require('debug')('t2-clapper');
+
 const sensor = ambient.use(tessel.port['A']);
 const TRIGGER_LEVEL = 0.13;
 const CLAP_THRESHOLD_MS = 1000;
@@ -14,12 +16,12 @@ let firstClap = false;
 const pub = new MulticastEmitter();
 
 sensor.on('ready', () => {
-  console.log('Setting sound trigger now');
+  debug('Setting sound trigger now');
 
   sensor.setSoundTrigger(TRIGGER_LEVEL);
 
   sensor.on('sound-trigger', sound => {
-    console.log('We hit the sound threshold:', sound);
+    debug('We hit the sound threshold:', sound);
     sensor.clearSoundTrigger(() => sensor.setSoundTrigger(TRIGGER_LEVEL));
 
     if (firstClap) {
@@ -31,11 +33,11 @@ sensor.on('ready', () => {
     }
 
     clapTimoutID = setTimeout(() => {
-      console.log('Clearing clapper');
+      debug('Clearing clapper');
       firstClap = false;
     }, CLAP_THRESHOLD_MS);
   });
 });
 
-sensor.on('error', console.error);
-pub.on('error', console.error);
+sensor.on('error', debug);
+pub.on('error', debug);
